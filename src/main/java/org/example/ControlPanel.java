a48d79ff282336dbc38911924ed9f3700146ef66
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-03-23 15:44:35 +0200

    Compulsory

---
+++ src/main/java/org/example/ControlPanel.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ControlPanel extends JPanel {
+    final MainFrame frame;
+    JButton saveBtn = new JButton("Save");
+    //create all buttons (Load, Reset, Exit)
+    JButton loadBtn = new JButton("Load");
+    JButton resetBtn = new JButton("Reset");
+    JButton exitBtn = new JButton("Exit");
+    //...TODO
+    public ControlPanel(MainFrame frame) {
+        this.frame = frame;
+        init();
+    }
+    private void init() {
+        //change the default layout manager (just for fun)
+        setLayout(new GridLayout(1, 4));
+        //add all buttons ...TODO
+        add(saveBtn);
+        add(loadBtn);
+        add(resetBtn);
+        add(exitBtn);
+        //configure listeners for all buttons
+        saveBtn.addActionListener(this::save);
+        //...TODO
+        loadBtn.addActionListener(this::load);
+        resetBtn.addActionListener(this::reset);
+        exitBtn.addActionListener(this::exit);
+    }
+    private void save(ActionEvent e) {
+        try {
+            ImageIO.write(frame.canvas.image, "PNG", new File("C:\\Users\\GeorgeS\\Documents\\facultate\\An 2\\cursuri\\sem2\\ProgramareAvansata\\test.png"));
+        } catch (IOException ex) { System.err.println(ex); }
+    }
+    //...TODO
+    private void load(ActionEvent e){
+        try{
+            BufferedImage x = ImageIO.read(new File("C:\\Users\\GeorgeS\\Documents\\facultate\\An 2\\cursuri\\sem2\\ProgramareAvansata\\test.png"));
+            frame.canvas.image=x;
+            frame.canvas.graphics=frame.canvas.image.createGraphics();
+            frame.repaint();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+    private void reset(ActionEvent e){
+        frame.canvas.reset();
+        frame.repaint();
+    }
+    private void exit(ActionEvent e){
+        frame.dispose();
+        System.exit(0);
+    }
+}
---
+++ src/main/java/org/example/MyComponent.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MyComponent extends JPanel {
+    private int x, y, radius;
+
+    public MyComponent() {
+        init();
+    }
+
+    private void init() {
+        setPreferredSize(new Dimension(400, 400));
+        this.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                x = e.getX();
+                y = e.getY();
+                radius = 50 + (int) (100 * Math.random());
+                repaint();
+            }
+        });
+    }
+    public void paintComponent (Graphics g){
+        super.paintComponent(g);
+        g.drawOval(x - radius / 2, y - radius / 2, radius, radius);
+    }
+}
+
+
